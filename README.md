c5de5470fc7e925ec611ccfda0eaa4a0cacc77e2
dwiknrd <dwiknrd@gmail.com>
2021-07-20 12:03:17 +0700

    add data and codes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# article-spinner
-Create Article Spinner AI
+# Article Spinner AI
+<hr>
+
+## 📌 Background
+Content marketing is a great technique to boost the brand’s web presence and attract potential customers or clients. When using blog posts or articles as an online marketing tool however, many companies can begin to feel like they’re being stretched thin. After all, even if you hire a skilled writer, quality content takes time and effort to produce.<br>
+
+Even though there are numerous methods that one can use to maximize on online visibility, spinning articles is one of the most effective ways to go about it. Article spinning involves creating new and unique articles from an original one. However, what matters most, in this instance, is to come up with unique and valuable content, and this boils down to the kind of software used.
+
+## 🙋 Audiences
+For blogger, SEO, digital marketer, website operator, or a writer that always lookout for unique content to post on your website. Benefit using article spinner: Cheap or zero cost, Time-saving, Unique output, and Improve search engines ranking.
+
+## 📝 AI Article Spinner:
+1. NLP (Natural Language Processing)
+2. Emulated Natural Language
+3. NLTK wordnet
+4. NLTK Rephrasing
---
+++ article-spinner.py
@@ -0,0 +1,87 @@
+import re
+import random
+import nltk
+nltk.download()
+from nltk.corpus import wordnet
+from nltk.tokenize import regexp_tokenize
+import nltk.data
+from nltk.stem.porter import *
+
+class spinner(object):
+
+    def spin(self, s):
+        while True:
+            s, n = re.subn('{([^{}]*)}',
+                lambda m: random.choice(m.group(1).split("|")), s)
+
+            if n == 0: break
+
+        return s.strip()
+
+
+    def splitToSentences(self, content):
+
+        tokenizer = nltk.data.load('nltk_data/punkt/english.pickle')
+
+        return tokenizer.tokenize(content)
+
+    
+    # get all synonyms of a word from the wordnet database
+    def getSynonyms(self, word):
+
+    #include the original word
+        synonyms = [word]
+        for syn in wordnet.synsets(word):
+            for lemma in syn.lemmas():
+                if lemma.name != word:
+                    # since wordnet lemma.name will include _ for spaces, we'll replace these with spaces
+                    w, n = re.subn("_", " ", lemma.name())
+                    synonyms.append(w)
+            s = list(set(synonyms))
+
+        return len(s), s
+
+    
+    """
+    Transform text into spintax with the folowing steps
+    1. split the text to sentences
+    2. loop through the sentences and tokenize it
+    3. loop thorugh each token, find its stem and assemble all the synonyms of it into the spintax
+    """
+
+    def getSpintax(self, text):
+        sentences = self.splitToSentences(text)
+        stemmer = PorterStemmer()
+        spintax = ""
+
+        for sentence in sentences:
+            tokens = regexp_tokenize(sentence, "[\w']+")
+
+            for token in tokens:
+                stem = token
+                n, syn = self.getSynonyms(stem)
+                spintax += "{"
+                spintax += token
+                spintax += "|"
+
+                for x in range(n):
+                    spintax += syn[x]
+                    if x < n-1:
+                        spintax += "|"
+                    else:
+                        spintax += "} "
+                
+        return spintax
+
+
+if __name__ == '__main__':
+# Assign the object for the class
+    s = spinner()
+    st = "Machine learning is a field of computer science that gives computer systems the ability to learn"
+    spintax = s.getSpintax(st)
+
+    print(spintax)
+
+    spun = s.spin(spintax)
+
+    print(spun)
